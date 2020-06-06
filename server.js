999f4e68401baf59ee819901523504b556350b36
Alejandro Martinez <alejomartinez8@gmail.com>
2020-06-06 13:32:29 -0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,21 @@ app.use(express.json({ extended: false }));
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API ejecutandose'));
-
 // Define Routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+
+// Serve static assets in productions
+if (process.env.NODE_EVN === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'intex.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
